94f50127c30f2c6a64fa72c84b38e5f085025605
Jasen Pan <jasenpan@gmail.com>
2021-06-18 17:09:42 +1000

    majoir

--- src/utils/stringHelpers.ts
+++ src/utils/stringHelpers.ts
@@ -1,7 +1,9 @@
-import { camelCase, snakeCase } from "lodash";
+import { camelCase, snakeCase, padEnd } from "lodash";
 
 export const reverseCamelCase = (str: string) =>
   camelCase(str).split("").reverse().join("").concat("------");
 
 export const reverseSnakeCase = (str: string) =>
   snakeCase(str).split("").reverse().join("").concat("------");
+
+export const addChar = (str: string) => padEnd(str, 10, "*");
