b0e29047b76bc755406d4c36a3d7065f431528d4
Jasen Pan <jasenpan@gmail.com>
2021-06-18 17:04:23 +1000

    add new feature

--- src/utils/stringHelpers.ts
+++ src/utils/stringHelpers.ts
@@ -1,4 +1,7 @@
-import { camelCase } from "lodash";
+import { camelCase, snakeCase } from "lodash";
 
 export const reverseCamelCase = (str: string) =>
   camelCase(str).split("").reverse().join("").concat("------");
+
+export const reverseSnakeCase = (str: string) =>
+  snakeCase(str).split("").reverse().join("").concat("------");
